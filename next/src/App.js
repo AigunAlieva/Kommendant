dd520c63ffeb288480f33a4ecdedffc05a2d4a16
Gunai Hasanbayli <gunaihasanbeyli@gmail.com>
2023-07-04 01:57:07 +0400

    aygun

--- next/src/App.js
+++ next/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import User from './User/user';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+       <Route   path="/" element={  <User/>}/>   
+      </Routes>
+      </BrowserRouter>
 
-<User/>
+
     </div>
   );
 }
